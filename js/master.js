5e967d579705ff53faa8e59befbd245bb4f931f3
労作 <zuorinojinri@gmail.com>
2025-11-26 21:28:35 +0900

    Add files via upload

--- js/master.js
+++ js/master.js
@@ -4,4 +4,5 @@ responsive_menu_btn.addEventListener('click', menuToggle);
 function menuToggle() {
   const header_menu_detail = document.querySelector('.main-nav');
   header_menu_detail.classList.toggle('menu_active');
+  responsive_menu_btn.classList.toggle('active');
 }
